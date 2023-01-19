368142b6798dd2e90aaa0dd20233a6924e65cbe3
Brent Grant <grant9788@gmail.com>
2023-01-18 21:32:37 -0500

    Add project files.

---
+++ Brent_Csharp_WPF/GUI/Customers.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Brent_Csharp_WPF.GUI
+{
+    /// <summary>
+    /// Interaction logic for Customers.xaml
+    /// </summary>
+    public partial class Customers : Window
+    {
+        public Customers()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Brent_Csharp_WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Brent_Csharp_WPF.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Brent_Csharp_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
+        }
+        private void CustomerButton(object sender, RoutedEventArgs e)
+        {
+            Customers aForm = new Customers();  //Creates an instance of an object to make the Customers GUI show up under new name
+            aForm.ShowDialog();
+        }
+    }
+}
